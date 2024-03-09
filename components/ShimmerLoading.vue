2a63f12a173bea59fe4a0776f38e92e688dc67ba
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-03-09 17:44:40 +0300

    ShimmerLoading component completed

---
+++ components/ShimmerLoading.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="overflow-hidden">
+    <div
+      class="w-full h-full relative overflow-hidden shimmer"
+      :style="{ '--background': props.background, '--shimmer': props.shimmer }"></div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  background: {
+    type: String,
+    default: '#c2c2c2'
+  },
+  shimmer: {
+    type: String,
+    default: '#ffffff'
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.shimmer {
+  --background: #c2c2c2;
+  --shimmer: #ffffff;
+  background-color: var(--background);
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 300%;
+    width: 100%;
+    animation: shimmer 2s infinite;
+    background: linear-gradient(90deg, transparent, var(--shimmer), transparent);
+  }
+}
+
+@keyframes shimmer {
+  from {
+    transform: rotate(30deg) translateX(-200%);
+  }
+  to {
+    transform: rotate(30deg) translateX(200%);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="w-full h-full min-h-[100vh] flex flex-col gap-3 justify-center items-center"></div>
+  <div class="w-full h-full min-h-[100vh] flex flex-col gap-3 justify-center items-center">
+    <ShimmerLoading class="w-52 h-72 rounded-xl"></ShimmerLoading>
+  </div>
 </template>
 
 <script setup></script>
