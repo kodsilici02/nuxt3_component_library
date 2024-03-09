b9c67cbc481c7f14783d97e9dd52a65b5dea1925
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-03-09 19:54:59 +0300

    SearchBarAnimated component completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="w-full h-full min-h-[100vh] flex flex-col gap-3 justify-center items-center">
-    <TooltipAnimated>
-      <template v-slot:tooltip>
-        <ButtonUnderGradient><div class="w-fit flex whitespace-nowrap">Tailwind CSS</div></ButtonUnderGradient>
-      </template>
-      <template v-slot:content>
-        <img src="/profile.png" class="w-12 h-12 rounded-full object-cover object-center" />
-      </template>
-    </TooltipAnimated>
+    <SearchBarAnimated></SearchBarAnimated>
   </div>
 </template>
 
