3db8a8762d1dc06faea6c0017c6c88bc1b53fe6c
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-03-09 18:10:04 +0300

    LambSection component completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-full min-h-[100vh] flex flex-col gap-3 justify-center items-center">
-    <ShimmerLoading class="w-52 h-72 rounded-xl"></ShimmerLoading>
+    <LambSection></LambSection>
   </div>
 </template>
 
