447ae1dd86a86543224b14e9b5c79a01c46dcd1a
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-03-20 23:56:56 +0300

    Sidebar component completed

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="w-full h-[200vh] flex flex-col gap-3 justify-start items-center mt-10">
-    <BentoContainer class="w-[50%] h-52">
-      <BentoGrid></BentoGrid>
-    </BentoContainer>
+  <div class="w-full h-[100vh] flex flex-col gap-3 justify-center items-center mt-10">
+    <button @click="deneme">deneme</button>
+    <SideBar :open="open" @handle-close="open = false"></SideBar>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const open = ref(false);
+function deneme() {
+  open.value = true;
+}
+</script>
 
 <style lang="scss" scoped></style>
