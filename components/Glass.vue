408e578f2574b4e850e9c54885fd701460e0e810
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-03-21 16:13:21 +0300

    Glass component completed

---
+++ components/Glass.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="rounded-lg" :style="{ backdropFilter: `blur(${props.blur}px)`, background: props.background }"></div>
+</template>
+
+<script setup>
+const props = defineProps({
+  background: {
+    type: String,
+    default: 'transparent'
+  },
+  blur: {
+    type: Number,
+    default: 10
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="w-full h-[100vh] flex flex-col gap-3 justify-center items-center mt-10">
-    <button @click="deneme">deneme</button>
-    <SideBar :open="open" @handle-close="open = false"></SideBar>
+  <div
+    class="w-full h-[100vh] flex flex-col gap-3 justify-center items-center mt-10"
+    style="background-image: url('/profile.png'); background-repeat: no-repeat; background-position: center">
+    <Glass class="w-52 h-52 border"></Glass>
   </div>
 </template>
 
