20e58255b4693e454a979a9ece08465756ed4c33
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2024-04-13 12:36:21 +0300

    random

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-[100vh] flex flex-col gap-3 justify-center items-center mt-10">
-    <ButtonUnderline><div class="px-3 py-2">anan</div></ButtonUnderline>
+    <ButtonUnderline><div class="px-3 py-2">naber</div></ButtonUnderline>
   </div>
 </template>
 
